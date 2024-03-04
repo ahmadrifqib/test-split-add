fc70d4ddfafcd8c8b760260e1f8d089798e7d7e1
Ahmad Rifqi Baidhowi <ahmad.rifqi@outlook.co.id>
2024-03-04 21:54:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This testing package for split-add
---
+++ index.js
@@ -0,0 +1,10 @@
+// add = require("split-add");
+import numberAdd from "split-add";
+import zuperhub from "zuperhub";
+import warpcastAutoByman from "warpcast-auto-byman";
+
+const result = numberAdd(1234);
+console.log(result);
+
+console.log(zuperhub);
+console.log(warpcastAutoByman);
